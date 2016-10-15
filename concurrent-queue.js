3909c451af714f15a3c8b42ed599b96aa9636987
loganfreeman <shanhongcheng@hotmail.com>
2016-10-14 23:18:33 -0600

    Create concurrent-queue.js

---
+++ concurrent-queue.js
@@ -0,0 +1,40 @@
+export function queue<T, U>(
+  arr: Array<U>,
+  promiseProducer: (result: U) => Promise<T>,
+  concurrency: number = Infinity,
+): Promise<Array<T>> {
+  concurrency = Math.min(concurrency, arr.length);
+
+  // clone
+  arr = arr.slice();
+
+  const results = [];
+  let total = arr.length;
+  if (!total) {
+    return Promise.resolve(results);
+  }
+
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < concurrency; i++) {
+      next();
+    }
+
+    function next() {
+      const item = arr.shift();
+      const promise = promiseProducer(item);
+
+      promise.then(function(result) {
+        results.push(result);
+
+        total--;
+        if (total === 0) {
+          resolve(results);
+        } else {
+          if (arr.length) {
+            next();
+          }
+        }
+      }, reject);
+    }
+  });
+}
