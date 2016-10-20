c9d325123925f0a414aeca49a04980529f06b890
loganfreeman <shanhongcheng@hotmail.com>
2016-10-20 11:45:05 -0600

    Create error-notification.js

---
+++ error-notification.js
@@ -0,0 +1,15 @@
+// ----------------------------
+// Error notification methods
+// ----------------------------
+var beep = function() {
+  var os = require('os');
+  var file = 'gulp/error.wav';
+  if (os.platform() === 'linux') {
+    // linux
+    exec("aplay " + file);
+  } else {
+    // mac
+    console.log("afplay " + file);
+    exec("afplay " + file);
+  }
+};
