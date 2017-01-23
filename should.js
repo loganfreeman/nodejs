d2511d5795fcbfbb1973a28e70dc90fcb5559e9e
loganfreeman <shanhongcheng@hotmail.com>
2017-01-23 11:43:42 -0700

    Create should.js

---
+++ should.js
@@ -0,0 +1,13 @@
+/**
+ * Expose api via `Object#should`.
+ *
+ * @api public
+ */
+
+Object.defineProperty(Object.prototype, 'should', {
+  set: function(){},
+  get: function(){
+    return should(this);
+  },
+  configurable: true
+});
